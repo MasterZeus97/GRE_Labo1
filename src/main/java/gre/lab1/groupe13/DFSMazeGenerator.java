f8c17d46885902f1415d38af123df692eb2132a2
PrazTobie <tobie.praz@heig-vd.ch>
2023-03-17 14:52:55 +0100

    Maze Gen

--- src/main/java/gre/lab1/groupe13/DFSMazeGenerator.java
+++ src/main/java/gre/lab1/groupe13/DFSMazeGenerator.java
@@ -1,8 +1,13 @@
 // TODO: refactor le nom du package groupeX avec le bon numéro de groupe (SHIFT + F6)
 package gre.lab1.groupe13;
 
+import gre.lab1.graph.Graph;
 import gre.lab1.gui.MazeGenerator;
 import gre.lab1.gui.MazeBuilder;
+import gre.lab1.gui.Progression;
+
+import java.util.Collections;
+import java.util.HashSet;
 
 // TODO: javadoc
 public final class DFSMazeGenerator implements MazeGenerator {
@@ -15,5 +20,27 @@ public final class DFSMazeGenerator implements MazeGenerator {
     //  - PROCESSED, en post-traitement.
     //  Le labyrinthe n'a que des murs au début de la construction, il faut donc créer les passages en
     //  supprimant des murs.
+
+    var visted = new HashSet<Integer>();
+    dfs(builder, visted, from);
+  }
+
+
+  void dfs(MazeBuilder builder, HashSet<Integer> visited, int node) {
+    visited.add(node);
+    builder.progressions().setLabel(node, Progression.PROCESSING);
+
+    var neighbors = builder.topology().neighbors(node);
+
+    Collections.shuffle(neighbors);
+
+    for (var neighbor : neighbors) {
+      if(!visited.contains(neighbor)) {
+        builder.removeWall(node, neighbor);
+        visited.add(neighbor);
+        dfs(builder, visited, neighbor);
+      }
+    }
+    builder.progressions().setLabel(node, Progression.PROCESSED);
   }
 }
